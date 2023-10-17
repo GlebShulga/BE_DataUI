35e66fd4fbacebf5f98aad6dac33ced2eebe112e
GlebShulga <Shulga_Gleb@hotmail.com>
2023-10-17 08:58:01 +0400

    change error number in isAdmin function

--- src/middleware/isAdmin.ts
+++ src/middleware/isAdmin.ts
@@ -1,12 +1,12 @@
 import { Request, Response, NextFunction } from "express";
-import { RESPONSE_CODE_FORBIDDEN } from "../constants/responseCodes";
+import { RESPONSE_CODE_UNAUTHORIZED } from "../constants/responseCodes";
 
 export function isAdmin(req: Request, res: Response, next: NextFunction) {
   const currentUser = req.user;
 
   if (currentUser.role !== "admin") {
     return res
-      .status(RESPONSE_CODE_FORBIDDEN)
+      .status(RESPONSE_CODE_UNAUTHORIZED)
       .send("Only admin can delete cart");
   }
   next();
